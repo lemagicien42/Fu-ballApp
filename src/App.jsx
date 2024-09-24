6fc6c3090685ec10d5e20e2ab1dac4c50fa17139
lemagicien42 <totosuper40@gmail.com>
2024-09-24 13:47:56 +0200

    renouvlerai

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Header from './componente/Header';;
 import Home from './componente/Home';
 import CategoryPage from './componente/CategoryPage';
@@ -7,16 +7,15 @@ import Footer from './componente/Footer';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/category/:categoryName" element={<CategoryPage />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+   
+  <div className="App">
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/category/:categoryName" element={<CategoryPage />} />
+    </Routes>
+    <Footer />
+  </div>
   );
 }
 
